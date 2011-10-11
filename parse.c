da1d2dd99e6702d711cea18b67c8619a436a2a1c
Diego Elio Pettenò <flameeyes@flameeyes.eu>
2011-10-11 20:00:58 +0200

    Add missing license headers to files.

--- parse.c
+++ parse.c
@@ -1,7 +1,21 @@
-/* ---------------------------------------------------------------------------
-unpaper - written by Jens Gulden 2005-2007                                  */
-
-/* ------------------------------------------------------------------------ */
+/* 
+ * This file is part of Unpaper.
+ *
+ * Copyright © 2005-2007 Jens Gulden
+ * Copyright © 2011-2011 Diego Elio Pettenò
+ *
+ * Unpaper is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * Unpaper is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
 
 /* --- tool functions for parameter parsing and verbose output ------------ */
 
