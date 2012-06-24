4ed1d12d249f846400438edf279b05a95c6a9db9
Diego Elio Pettenò <flameeyes@flameeyes.eu>
2012-06-24 07:46:58 +0200

    tests: remote extra whiteline.

--- tests/runtestF1.sh
+++ tests/runtestF1.sh
@@ -5,7 +5,6 @@ echo "[F1] Merging 2-page layout into single output page (with input and output 
 set -e
 set -x
 
-
 rm -f tests/resultsF11.pbm
 
 ./unpaper --end-sheet 1 -v --layout double --input-pages 2 tests/imgsrcE%03d.pbm tests/resultsF1%d.pbm
