ecbb9d00d7bd298b281efc3fbfa3fdff5bc9024a
Diego Elio Pettenò <flameeyes@flameeyes.eu>
2014-08-08 19:21:58 +0100

    Input and output match again now, so remove the bogus NEWS entry.
    Also revert the tests so that they don't pass the --depth option at all.

--- tests/runtestC1.sh
+++ tests/runtestC1.sh
@@ -6,6 +6,6 @@ set -e
 set -x
 
 rm -f tests/resultsC1.pbm
-./unpaper -v -n --sheet-size a4 --sheet-background black --depth 1 ${srcdir:-.}/tests/imgsrc002.png tests/resultsC1.pbm
+./unpaper -v -n --sheet-size a4 --sheet-background black ${srcdir:-.}/tests/imgsrc002.png tests/resultsC1.pbm
 
 [ -f tests/resultsC1.pbm ]
--- tests/runtestC2.sh
+++ tests/runtestC2.sh
@@ -6,6 +6,6 @@ set -e
 set -x
 
 rm -f tests/resultsC2.pbm
-./unpaper -v -n --sheet-size a4 --pre-shift -5cm,9cm --depth 1 ${srcdir:-.}/tests/imgsrc002.png tests/resultsC2.pbm
+./unpaper -v -n --sheet-size a4 --pre-shift -5cm,9cm ${srcdir:-.}/tests/imgsrc002.png tests/resultsC2.pbm
 
 [ -f tests/resultsC2.pbm ]
