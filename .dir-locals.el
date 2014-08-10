fcf12e5f9437ed2996baba552444ebebccf66fbe
Diego Elio Pettenò <flameeyes@flameeyes.eu>
2014-08-10 12:53:13 +0100

    Move the local variables settings for nxml to .dir-locals.
    This also adds flyspell by default for markdown-mode.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,6 @@
 ((nil . ((indent-tabs-mode . nil)))
- (c-mode . ((c-basic-offset . 4))))
+ (c-mode . ((c-basic-offset . 4)))
+ (nxml-mode . ((fill-column . 100)
+               (mode . auto-fill)
+               (mode . flyspell)))
+ (markdown-mode . ((mode . flyspell))))
