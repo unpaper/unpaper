24ada3131d753dff49c64288a79728bef3726631
Diego Elio Pettenò <flameeyes@flameeyes.com>
2020-04-19 15:05:51 +0100

    Actually use MIT rather than CC0, for OSI compatibility.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,6 @@
 ; SPDX-FileCopyrightText: 2005 The unpaper authors
 ;
-; SPDX-License-Identifier: CC0-1.0
+; SPDX-License-Identifier: MIT
 
 ((nil . ((indent-tabs-mode . nil)))
  (c-mode . ((c-basic-offset . 4)))
