7e9ed84a7640a3b86d53a2591cb27458b7098b82
Diego Elio Pettenò <flameeyes@flameeyes.com>
2021-01-30 16:26:37 +0000

    Convert man page from DocBook to ReStructured Text.
    This depends on Sphinx for the generation of the man page, instead of
    using the `xsltproc` with the DocBook source file.
    
    Installation of the man page is enabled by default via Meson, and
    should be equivalent to the previous automake installation.
    
    Address the first part of issue #76.

---
+++ doc/conf.py
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: 2020 The unpaper Authors
+#
+# SPDX-License-Identifier: MIT
+
+project = "unpaper"
+copyright = "2020, The unpaper Authors"
+author = "The unpaper Authors"
+
+man_pages = [
+    ("unpaper.1", "unpaper", "unpaper", "The unpaper authors", 1),
+]
