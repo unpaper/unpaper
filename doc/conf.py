c490f7466cfd947dedea6326ed41135dc08b2abe
Diego Elio Pettenò <flameeyes@flameeyes.com>
2021-05-16 17:41:45 +0100

    Disable the man page section directory generation.
    This makes Sphinx 4.0.0 and 4.0.1 work again to build unpaper. The
    default has been turned back off for 4.0.2 (not released yet) but it
    might be turned on by default in 4.1.0 again.

--- doc/conf.py
+++ doc/conf.py
@@ -6,6 +6,11 @@ project = "unpaper"
 copyright = "2020, The unpaper Authors"
 author = "The unpaper Authors"
 
+# Avoid generate sub-directory for the man pages during build:
+# https://github.com/sphinx-doc/sphinx/issues/9217
+# This appears incompatible with the Meson expectation.
+man_make_section_directory = False
+
 man_pages = [
     ("unpaper.1", "unpaper", "unpaper", "The unpaper authors", 1),
 ]
