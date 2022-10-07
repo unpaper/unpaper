7466eed62d2ba538ad0a5f5dfa6c028cf134ac86
Diego Elio Pettenò <flameeyes@flameeyes.com>
2022-10-07 19:48:42 +0100

    Update editor hints to use 2-space indent.
    This match the default of clang-format.

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,7 +3,7 @@
 ; SPDX-License-Identifier: MIT
 
 ((nil . ((indent-tabs-mode . nil)))
- (c-mode . ((c-basic-offset . 4)))
+ (c-mode . ((c-basic-offset . 2)))
  (nxml-mode . ((fill-column . 100)
                (mode . auto-fill)
                (mode . flyspell)))
