6dc8315d4b6283d5c18384fa5cdd6d6f1e429e93
Diego Elio Pettenò <flameeyes@flameeyes.com>
2024-01-14 18:14:54 +0000

    Start refactoring APIs away from global variables.
    While this is leading to some duplication for the time being, we can
    start separating the logic to not depend on the global variables.

---
+++ imageprocess/pixel.h
@@ -0,0 +1,28 @@
+// SPDX-FileCopyrightText: 2005 The unpaper authors
+//
+// SPDX-License-Identifier: GPL-2.0-only
+
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include <libavutil/frame.h>
+
+typedef struct {
+  int32_t x;
+  int32_t y;
+} Point;
+
+typedef struct {
+  uint8_t r;
+  uint8_t g;
+  uint8_t b;
+} Pixel;
+
+Pixel get_pixel(AVFrame *image, Point coords);
+uint8_t get_pixel_grayscale(AVFrame *image, Point coords);
+uint8_t get_pixel_lightness(AVFrame *image, Point coords);
+uint8_t get_pixel_darkness_inverse(AVFrame *image, Point coords);
+bool set_pixel(AVFrame *image, Point coords, Pixel pixel,
+               uint8_t abs_black_threshold);
