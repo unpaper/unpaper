2264850868d06c532a11b3f264064f6ff381ff77
Diego Elio Pettenò <flameeyes@flameeyes.com>
2024-01-20 12:21:58 +0000

    Introduce some build support scripts.
    This includes native machine files for GCC and CLANG builds, plus
    some scripts to build and test them all.

---
+++ build-scripts/setup-all.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# SPDX-FileCopyrightText: 2024 The unpaper authors
+#
+# SPDX-License-Identifier: MIT
+
+if git status &>/dev/null; then
+  ROOT=$(git rev-parse --show-toplevel)
+elif sl root &>/dev/null; then
+  ROOT=$(sl root)
+else
+  echo "Unable to detect SCM." >&2
+  exit 1
+fi
+
+cd "$ROOT"
+for file in meson-environments/*.ini; do
+  environment=$(basename "${file}" .ini)
+
+  meson setup --reconfigure --native-file "${file}" "builddir/${environment}" || exit $?
+done
