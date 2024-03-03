11d08061454911def445fb75897659ceac08175e
Diego Elio Pettenò <flameeyes@flameeyes.com>
2024-03-03 13:15:53 +0000

    Nearly complete the migration of all options to the Options structure.
    This still exclude the options that need to be allocated,
    but at least it makes it possible to keep the various
    options parsing into a single structure from here on.

--- lib/options.h
+++ lib/options.h
@@ -6,6 +6,8 @@
 
 #include <stdbool.h>
 
+#include <libavutil/pixfmt.h>
+
 #include "constants.h"
 #include "imageprocess/deskew.h"
 #include "imageprocess/filters.h"
@@ -15,6 +17,11 @@
 #include "parse.h"
 
 typedef struct {
+  bool write_output;
+  bool overwrite_output;
+  bool multiple_sheets;
+  enum AVPixelFormat output_pixel_format;
+
   Layout layout;
   int start_sheet;
   int end_sheet;
@@ -45,23 +52,40 @@ typedef struct {
   Delta pre_shift;
   Delta post_shift;
 
+  int16_t pre_rotate;
+  int16_t post_rotate;
+
+  Direction pre_mirror;
+  Direction post_mirror;
+
   RectangleSize sheet_size;
   RectangleSize page_size;
   RectangleSize post_page_size;
   RectangleSize stretch_size;
   RectangleSize post_stretch_size;
 
+  float pre_zoom_factor;
+  float post_zoom_factor;
+
+  Pixel sheet_background;
+  Pixel mask_color;
   uint8_t abs_black_threshold;
   uint8_t abs_white_threshold;
+
+  Border pre_border;
+  Border border;
+  Border post_border;
 
   DeskewParameters deskew_parameters;
   MaskDetectionParameters mask_detection_parameters;
   MaskAlignmentParameters mask_alignment_parameters;
   BorderScanParameters border_scan_parameters;
 
+  Interpolation interpolate_type;
   GrayfilterParameters grayfilter_parameters;
   BlackfilterParameters blackfilter_parameters;
   BlurfilterParameters blurfilter_parameters;
+  uint64_t noisefilter_intensity;
 } Options;
 
 void options_init(Options *o);
